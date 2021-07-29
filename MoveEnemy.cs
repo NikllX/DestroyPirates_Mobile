2a5c91850a3c21c353a8a539a888d17f6ca19f0b
Nikita <61244906+NikllX@users.noreply.github.com>
2021-07-29 16:41:44 +0300

    Движение врага

---
+++ MoveEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public Vector2 direction;
+    public float moveSpeed = 1;
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * moveSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D ColAsteroid)
+    {
+        if (ColAsteroid.gameObject.tag == "Bullet")
+        {
+            Destroy(ColAsteroid.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
